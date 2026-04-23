a19c48d8ae89344b375af7bbb100ee2494fbc183
Darya Namazova <149301408+b2230356184@users.noreply.github.com>
2026-04-23 10:05:27 +0300

    Basic grid and item

--- src/main/java/utils/Grid.java
+++ src/main/java/utils/Grid.java
@@ -1,34 +1,54 @@
 package utils;
 
-public class Grid {
+import java.util.List;
+
+import com.kurawler.model.Item;
+
+
+
+
+public class Grid{
 	
-	// 2D array 
-    private Item[][] grid;
-    private final int WIDTH = 10;
-    private final int HEIGHT = 10;
-
+	
+	protected Tile[][] grid;
+    protected final int WIDTH = 100;
+    protected final int HEIGHT = 100;
+  
     public Grid() {
-        this.grid = new Item[WIDTH][HEIGHT];
-    }
+        grid = new Tile[WIDTH][HEIGHT];
 
+       
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                grid[x][y] = new Tile();
+            }
+        }
+    }
+    
+    public boolean isInside(int x, int y) {
+        return x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT;
+    }
+    
     public boolean isWalkable(int x, int y) {
-        // Map Boundary Check
-        if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT) {
-            return false;
-        }
+        if (!isInside(x, y)) return false;
 
-        Item objectAtTile = grid[x][y];
-
-        // If the tile is empty, the hero can walk there
-        if (objectAtTile == null) {
-            return true;
-        }
-
-        // Detect object type by referencing the Item class field
-        if (objectAtTile.getType().equalsIgnoreCase("STATIC")) {
-            return false; 
-        }
-
-        return true;
+        return grid[x][y].isWalkable();
     }
+    
+    public Tile getTile(int x, int y) {
+        if (!isInside(x, y)) return null;
+        return grid[x][y];
+    }
+    
+    public void placeItem(int x, int y, Item item) {
+        if (isInside(x, y)) {
+            grid[x][y].setItem(item);
+        }
+    }
+    public void removeItem(int x, int y) {
+        if (isInside(x, y)) {
+            grid[x][y].removeItem();
+        }
+    }
+    
 }
